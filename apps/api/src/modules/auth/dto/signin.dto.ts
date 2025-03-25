5f50878720b8e82ff2f8a03ceedf6f7e0f221cc4
Lucas Franco <lcs.franco556@gmail.com>
2025-03-25 16:49:53 -0300

    feat: create signin and signup dtos

---
+++ apps/api/src/modules/auth/dto/signin.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
+
+export class SigninDto {
+  /**
+   * Email that the user will register
+   * @example mail@mail.com
+   */
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+
+  /**
+   * Password that the user will register
+   * OBS: The password must be at least 8 characters long
+   * @example Potato69
+   */
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string
+}
