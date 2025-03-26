b44e4565d874d9450eef5851ca53a89482b003c5
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-03-26 19:48:06 -0300

    feat: add landing page

--- apps/ui/src/Router.tsx
+++ apps/ui/src/Router.tsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { LandingPage } from "./pages/LandingPage";
 
 export const Router = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<DefaultLayout />}>
+				<Route index element={<LandingPage />} />
 			</Route>
 		</Routes>
 	);
