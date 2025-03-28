e80163022f73bf2d312008c15388e232a59173a9
Lucas Franco <lcs.franco556@gmail.com>
2025-03-28 00:20:11 -0300

    feat: add properties to create schedule dto

--- apps/api/src/modules/schedules/dto/create-schedule.dto.ts
+++ apps/api/src/modules/schedules/dto/create-schedule.dto.ts
@@ -1,1 +1,19 @@
-export class CreateScheduleDto {}
+import { IsDate, IsNotEmpty, IsString } from 'class-validator'
+
+export class CreateScheduleDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @IsString()
+  @IsNotEmpty()
+  user_id: string
+
+  @IsNotEmpty()
+  @IsDate()
+  start_date: Date
+
+  @IsNotEmpty()
+  @IsDate()
+  end_date: Date
+}
