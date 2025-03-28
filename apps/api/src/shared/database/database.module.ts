447d547a5133995e7356f95381fe97c4e3a9f08b
Lucas Franco <lcs.franco556@gmail.com>
2025-03-27 22:53:18 -0300

    feat: export department repository

--- apps/api/src/shared/database/database.module.ts
+++ apps/api/src/shared/database/database.module.ts
@@ -2,10 +2,11 @@ import { Global, Module } from '@nestjs/common'
 
 import { PrismaService } from './prisma.service'
 import { UsersRepository } from './repositories/users.repositories'
+import { DepartmentsRepository } from './repositories/departments.repositories'
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository],
-  exports: [UsersRepository],
+  providers: [PrismaService, UsersRepository, DepartmentsRepository],
+  exports: [UsersRepository, DepartmentsRepository],
 })
 export class DatabaseModule {}
