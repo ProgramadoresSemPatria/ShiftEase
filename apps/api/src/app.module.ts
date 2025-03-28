74e4f90c7a73ba9d227ad282eac6e8c7b5b7126b
Lucas Franco <lcs.franco556@gmail.com>
2025-03-27 23:03:53 -0300

    feat: import schedule module on app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,3 +1,5 @@
+import { DepartmentsModule } from '@modules/departments/departments.module'
+import { SchedulesModule } from '@modules/schedules/schedules.module'
 import { RoleGuard } from '@modules/users/roles/role.guard'
 import { UsersModule } from '@modules/users/users.module'
 import { Module } from '@nestjs/common'
@@ -5,10 +7,15 @@ import { APP_GUARD } from '@nestjs/core'
 import { DatabaseModule } from '@shared/database/database.module'
 import { AuthGuard } from './modules/auth/auth.guard'
 import { AuthModule } from './modules/auth/auth.module'
-import { DepartmentsModule } from '@modules/departments/departments.module'
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UsersModule, DepartmentsModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    UsersModule,
+    DepartmentsModule,
+    SchedulesModule,
+  ],
   controllers: [],
   providers: [
     {
