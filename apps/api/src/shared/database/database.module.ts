ac771de66dedb6d716b18712a4dd172fce5642fa
Lucas Franco <lcs.franco556@gmail.com>
2025-03-27 23:02:43 -0300

    feat: schedule repository onde db module

--- apps/api/src/shared/database/database.module.ts
+++ apps/api/src/shared/database/database.module.ts
@@ -1,12 +1,18 @@
 import { Global, Module } from '@nestjs/common'
 
 import { PrismaService } from './prisma.service'
-import { UsersRepository } from './repositories/users.repositories'
 import { DepartmentsRepository } from './repositories/departments.repositories'
+import { SchedulesRepository } from './repositories/schedules.repositories'
+import { UsersRepository } from './repositories/users.repositories'
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository, DepartmentsRepository],
-  exports: [UsersRepository, DepartmentsRepository],
+  providers: [
+    PrismaService,
+    UsersRepository,
+    DepartmentsRepository,
+    SchedulesRepository,
+  ],
+  exports: [UsersRepository, DepartmentsRepository, SchedulesRepository],
 })
 export class DatabaseModule {}
