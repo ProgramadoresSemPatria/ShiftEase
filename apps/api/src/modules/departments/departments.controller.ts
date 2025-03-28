fdb39e64edc95c4bce51205af807654e59f6a2c8
Lucas Franco <lcs.franco556@gmail.com>
2025-03-27 22:48:22 -0300

    feat: department controller

---
+++ apps/api/src/modules/departments/departments.controller.ts
@@ -0,0 +1,50 @@
+import { Role } from '@modules/users/roles/entities/Role'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common'
+import { IsPublic } from '@shared/decorators/is-public.decorator'
+import { NecessaryRole } from '@shared/decorators/roles.decorator'
+import { DepartmentsService } from './departments.service'
+import { CreateDepartmentDto } from './dto/create-department.dto'
+import { UpdateDepartmentDto } from './dto/update-department.dto'
+
+@Controller('departments')
+export class DepartmentsController {
+  constructor(private readonly departmentsService: DepartmentsService) {}
+
+  @NecessaryRole(Role.ADMIN)
+  @Post()
+  create(@Body() createDepartmentDto: CreateDepartmentDto) {
+    return this.departmentsService.create(createDepartmentDto)
+  }
+
+  @IsPublic()
+  @Get()
+  findAll() {
+    return this.departmentsService.findAll()
+  }
+
+  @NecessaryRole(Role.ADMIN)
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateDepartmentDto: UpdateDepartmentDto,
+  ) {
+    return this.departmentsService.update(id, updateDepartmentDto)
+  }
+
+  @NecessaryRole(Role.ADMIN)
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.departmentsService.remove(id)
+  }
+}
