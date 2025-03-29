bea050e9b1c7e10ecfa8e8a143cdd4d92167d5c9
Lucas Franco <lcs.franco556@gmail.com>
2025-03-28 22:58:54 -0300

    feat: shift module services

--- apps/api/src/modules/shifts/shifts.module.ts
+++ apps/api/src/modules/shifts/shifts.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
-import { ShiftsService } from './shifts.service'
+import { ShiftsService } from './services/shifts.service'
 import { ShiftsController } from './shifts.controller'
 import { DepartmentsModule } from '@modules/departments/departments.module'
+import { FindShiftService } from './services/findShift.service'
 
 @Module({
+  exports: [FindShiftService],
   imports: [DepartmentsModule],
   controllers: [ShiftsController],
-  providers: [ShiftsService],
+  providers: [ShiftsService, FindShiftService],
 })
 export class ShiftsModule {}
