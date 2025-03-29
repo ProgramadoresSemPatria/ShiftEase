d77daab31af1b5a02602985dd8064769554c28a9
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-03-29 00:55:59 -0300

    feat: add register page

--- apps/ui/src/Router.tsx
+++ apps/ui/src/Router.tsx
@@ -4,6 +4,7 @@ import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Dashboard } from "./pages/Dashboard";
 import { LandingPage } from "./pages/LandingPage";
 import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 export const Router = () => {
 	return (
@@ -13,6 +14,7 @@ export const Router = () => {
 					<Route index element={<LandingPage />} />
 				</Route>
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
 			</Route>
 
 			<Route element={<AuthGuard isPrivate />}>
--- apps/ui/src/contexts/auth/AuthProvider.tsx
+++ apps/ui/src/contexts/auth/AuthProvider.tsx
@@ -1,17 +1,19 @@
-import { useApi } from "@/hooks/useApi";
+import { useApiPublic } from "@/hooks/useApiPublic";
+import { useApiUser } from "@/hooks/useApiUser";
 import { type JSX, useEffect, useMemo, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import type { contextUser } from "./AuthContext";
 
 export const AuthProvider = ({ children }: { children: JSX.Element }) => {
 	const [user, setUser] = useState<contextUser | null>(null);
-	const api = useMemo(() => useApi(), []);
+	const apiUser = useMemo(() => useApiUser(), []);
+	const apiPublic = useApiPublic();
 
 	useEffect(() => {
 		const validateToken = async () => {
 			const token = localStorage.getItem("accessToken");
 			if (token) {
-				const data = await api.validateToken();
+				const data = await apiUser.validateToken();
 				if (data) {
 					setUser(data);
 				} else {
@@ -20,17 +22,17 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
 			}
 		};
 		validateToken();
-	}, [api]);
+	}, [apiUser]);
 
 	const setToken = (token: string) => {
 		localStorage.setItem("accessToken", token);
 	};
 
 	const signIn = async (email: string, password: string) => {
-		const data = await api.signIn(email, password);
+		const data = await apiPublic.signIn(email, password);
 		if (data?.accessToken) {
 			setToken(data.accessToken);
-			const userData = await api.validateToken();
+			const userData = await apiUser.validateToken();
 			if (userData) {
 				setUser(userData);
 				return true;
@@ -39,14 +41,31 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
 		return false;
 	};
 
-	const signOut = async () => {
+	const signOut = () => {
 		setUser(null);
 		localStorage.removeItem("accessToken");
-		await api.signOut();
+	};
+
+	const signUp = async (
+		name: string,
+		email: string,
+		password: string,
+		departmentCode: string,
+	) => {
+		const data = await apiPublic.signUp(name, email, password, departmentCode);
+		if (data?.accessToken) {
+			setToken(data.accessToken);
+			const userData = await apiUser.validateToken();
+			if (userData) {
+				setUser(userData);
+				return true;
+			}
+		}
+		return false;
 	};
 
 	return (
-		<AuthContext.Provider value={{ user, signIn, signOut }}>
+		<AuthContext.Provider value={{ user, signIn, signOut, signUp }}>
 			{children}
 		</AuthContext.Provider>
 	);
