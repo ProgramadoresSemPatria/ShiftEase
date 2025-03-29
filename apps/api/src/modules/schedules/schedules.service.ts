6f57a7d4fe7cc2dde41b53eac080f75c785a0ad7
Lucas Franco <lcs.franco556@gmail.com>
2025-03-29 17:01:02 -0300

    feat: schedule service

--- apps/api/src/modules/schedules/schedules.service.ts
+++ apps/api/src/modules/schedules/schedules.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
 import { SchedulesRepository } from '@shared/database/repositories/schedules.repositories'
 import { CreateScheduleDto } from './dto/create-schedule.dto'
 import { UpdateScheduleDto } from './dto/update-schedule.dto'
@@ -19,18 +23,31 @@ export class SchedulesService {
   }
 
   findAll() {
-    return `This action returns all schedules`
+    return this.schedulesRepo.findMany({})
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} schedule`
+  async findOne(id: string) {
+    const schedule = await this.schedulesRepo.findUnique({ where: { id } })
+    if (!schedule) throw new NotFoundException('Schedule not found')
   }
 
   update(id: string, updateScheduleDto: UpdateScheduleDto) {
-    return `This action updates a #${id} schedule`
+    const { name, userId, startDate, endDate } = updateScheduleDto
+    if (startDate && endDate)
+      if (new Date(startDate) >= new Date(endDate))
+        throw new BadRequestException(
+          'Start date must be earlier then end date',
+        )
+
+    return this.schedulesRepo.update({
+      data: { name, user_id: userId, start_date: startDate, end_date: endDate },
+      where: { id },
+    })
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} schedule`
+  async remove(id: string) {
+    await this.schedulesRepo.delete({ where: { id } })
+
+    return
   }
 }
