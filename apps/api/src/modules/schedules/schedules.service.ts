5cecddf5cfe24abb114ee2b638a7c5562e97eb6e
Lucas Franco <lcs.franco556@gmail.com>
2025-03-29 01:04:28 -0300

    feat: create schedule method

--- apps/api/src/modules/schedules/schedules.service.ts
+++ apps/api/src/modules/schedules/schedules.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { SchedulesRepository } from '@shared/database/repositories/schedules.repositories'
 import { CreateScheduleDto } from './dto/create-schedule.dto'
 import { UpdateScheduleDto } from './dto/update-schedule.dto'
 
 @Injectable()
 export class SchedulesService {
+  constructor(private readonly schedulesRepo: SchedulesRepository) {}
+
   create(createScheduleDto: CreateScheduleDto) {
-    const { name, user_id, start_date, end_date } = createScheduleDto
+    const { name, userId, startDate, endDate } = createScheduleDto
+
+    if (new Date(startDate) >= new Date(endDate))
+      throw new BadRequestException('Start date must be earlier then end date')
+
+    return this.schedulesRepo.create({
+      data: { name, user_id: userId, start_date: startDate, end_date: endDate },
+    })
   }
 
   findAll() {
