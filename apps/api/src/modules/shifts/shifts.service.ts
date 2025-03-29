687f10bb4d8313be40fa9725b777cdfdbd0bc6bd
Lucas Franco <lcs.franco556@gmail.com>
2025-03-28 21:59:13 -0300

    feat: create shift method

--- apps/api/src/modules/shifts/shifts.service.ts
+++ apps/api/src/modules/shifts/shifts.service.ts
@@ -1,10 +1,38 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { CreateShiftDto } from './dto/create-shift.dto'
 import { UpdateShiftDto } from './dto/update-shift.dto'
+import { ShiftsRepository } from '@shared/database/repositories/shifts.repositories'
+import { FindUniqueDepartment } from '@modules/departments/services/findUniqueDepartmet.service'
 
 @Injectable()
 export class ShiftsService {
-  create(createShiftDto: CreateShiftDto) {}
+  constructor(
+    private readonly shiftsRepo: ShiftsRepository,
+    private readonly findUniqueDepartmet: FindUniqueDepartment,
+  ) {}
+
+  async create(createShiftDto: CreateShiftDto) {
+    const { departmentCode, type } = createShiftDto
+
+    const departmentExists = await this.findUniqueDepartmet.find(
+      undefined,
+      departmentCode,
+    )
+
+    if (!departmentExists) throw new NotFoundException('Department not found')
+
+    const shift = this.shiftsRepo.create({
+      data: {
+        department_id: departmentExists.id,
+        type,
+      },
+      include: {
+        department: true,
+      },
+    })
+
+    return shift
+  }
 
   findAll() {
     return `This action returns all shifts`
