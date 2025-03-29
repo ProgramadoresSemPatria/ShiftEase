cde99137679d0f4887936f27f10a23e6b2afd3bd
Lucas Franco <lcs.franco556@gmail.com>
2025-03-28 21:58:49 -0300

    feat: import department module

--- apps/api/src/modules/shifts/shifts.module.ts
+++ apps/api/src/modules/shifts/shifts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ShiftsService } from './shifts.service'
 import { ShiftsController } from './shifts.controller'
+import { DepartmentsModule } from '@modules/departments/departments.module'
 
 @Module({
+  imports: [DepartmentsModule],
   controllers: [ShiftsController],
   providers: [ShiftsService],
 })
