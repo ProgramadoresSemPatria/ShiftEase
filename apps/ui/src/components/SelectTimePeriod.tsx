6b87e616b747d9a407854ddf9a8e8b15e78795e2
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-03-29 23:46:38 -0300

    feat: add time period components

---
+++ apps/ui/src/components/SelectTimePeriod.tsx
@@ -0,0 +1,78 @@
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export default function SelectTimePeriod() {
+	const [dateFrom, setDateFrom] = React.useState<Date>();
+	const [dateTo, setDateTo] = React.useState<Date>();
+
+	return (
+		<div className="flex flex-col md:flex-row gap-3 md:items-end">
+			<div className="flex flex-col">
+				<span className=" text-md text-gray-700">From:</span>
+				<Popover>
+					<PopoverTrigger asChild>
+						<Button
+							variant={"outline"}
+							className={cn(
+								"w-[280px] justify-start text-left font-normal",
+								!dateFrom && "text-muted-foreground",
+							)}
+						>
+							<CalendarIcon className="mr-2 h-4 w-4" />
+							{dateFrom ? format(dateFrom, "PPP") : <span>Pick a date</span>}
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent className="w-auto p-0">
+						<Calendar
+							mode="single"
+							selected={dateFrom}
+							onSelect={setDateFrom}
+							initialFocus
+						/>
+					</PopoverContent>
+				</Popover>
+			</div>
+			<div className="flex flex-col">
+				<span className=" text-md text-gray-700">To:</span>
+				<Popover>
+					<PopoverTrigger asChild>
+						<Button
+							variant={"outline"}
+							className={cn(
+								"w-[280px] justify-start text-left font-normal",
+								!dateTo && "text-muted-foreground",
+							)}
+						>
+							<CalendarIcon className="mr-2 h-4 w-4" />
+							{dateTo ? format(dateTo, "PPP") : <span>Pick a date</span>}
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent className="w-auto p-0">
+						<Calendar
+							mode="single"
+							selected={dateTo}
+							onSelect={setDateTo}
+							initialFocus
+						/>
+					</PopoverContent>
+				</Popover>
+			</div>
+			{/* <button
+				type="button"
+				className="bg-black text-white rounded-md py-2 px-4 w-fit h-fit font-semibold hover:bg-orange-500 cursor-pointer"
+			>
+				Filter
+			</button> */}
+		</div>
+	);
+}
