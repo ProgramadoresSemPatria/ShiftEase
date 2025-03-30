900f681b16ba9deb86a7799f4f63afca98259291
Lucas Franco <lcs.franco556@gmail.com>
2025-03-30 15:06:55 -0300

    fix: swap schedule

--- apps/api/src/modules/schedule-shifts/schedule-shifts.service.ts
+++ apps/api/src/modules/schedule-shifts/schedule-shifts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { ScheduleShiftRepository } from '@shared/database/repositories/schedule-shift.repositories'
 
 @Injectable()
@@ -10,9 +10,18 @@ export class ScheduleShiftsService {
   }
 
   async swapScheduleShift(originId: string, destinationId: string) {
+    const destinationScheduleShift = await this.scheduleShiftsRepo.findUnique({
+      where: { id: destinationId },
+      select: { shift_id: true },
+    })
+
+    if (!destinationScheduleShift) {
+      throw new NotFoundException('Destination ScheduleShift not found')
+    }
+
     return this.scheduleShiftsRepo.update({
       where: { id: originId },
-      data: { shift_id: destinationId },
+      data: { shift_id: destinationScheduleShift.shift_id },
     })
   }
 }
