756f3effef5c7968cd2e580bd5d4f4704becc2f3
Lucas Franco <lcs.franco556@gmail.com>
2025-03-29 22:05:22 -0300

    feat: swap request service

---
+++ apps/api/src/modules/shift-exchange-request/shift-exchange-request.service.ts
@@ -0,0 +1,35 @@
+import { UsersService } from '@modules/users/users.service'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { ShiftExchangeRequestRepository } from '@shared/database/repositories/shift-exchange-requests.repositories'
+import { CreateShiftExchangeRequestDto } from './dto/create-shit-exchange-request.dto'
+
+@Injectable()
+export class ShiftExchangeRequestService {
+  constructor(
+    private readonly shiftExchangeRequestRepo: ShiftExchangeRequestRepository,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async create(
+    createShiftExchangeRequestDto: CreateShiftExchangeRequestDto,
+    userId: string,
+  ) {
+    const { receptorId, departmentId, originShiftId, destinationId, reason } =
+      createShiftExchangeRequestDto
+
+    const receptorIdExists = await this.usersService.getUserById(receptorId)
+    if (!receptorIdExists)
+      throw new NotFoundException('Receptor user not found')
+
+    return this.shiftExchangeRequestRepo.create({
+      data: {
+        applicant_id: userId,
+        receptor_id: receptorId,
+        department_id: departmentId,
+        origin_shift_id: originShiftId,
+        destination_id: destinationId,
+        reason,
+      },
+    })
+  }
+}
