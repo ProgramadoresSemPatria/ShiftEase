4af383dfb5aaa4fdd67578e9de2f9596e55b3fd2
Lucas Franco <lcs.franco556@gmail.com>
2025-03-30 20:42:27 -0300

    feat: schedules contexts

---
+++ apps/ui/src/hooks/useApiSchedules.ts
@@ -0,0 +1,31 @@
+import { api } from "@/services/api";
+import type { ShiftType } from "@/types/enums";
+import type { Schedule } from "@/types/schedule";
+
+export type QueryParams = {
+	search: string;
+	departmentId: string;
+	shiftType: ShiftType;
+	from: string;
+	to: string;
+};
+
+export const useApiSchedules = () => ({
+	findAll: async ({
+		search,
+		departmentId,
+		shiftType,
+		from,
+		to,
+	}: QueryParams): Promise<Schedule | null> => {
+		try {
+			const response = await api.get("/schedules", {
+				params: { search, departmentId, shiftType, from, to },
+			});
+			return response.data;
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	},
+});
