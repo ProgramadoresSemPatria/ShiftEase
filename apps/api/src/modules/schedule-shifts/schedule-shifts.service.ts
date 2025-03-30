571c25d8bc0ef27be0b66a7a7c95230dfb22abcc
Lucas Franco <lcs.franco556@gmail.com>
2025-03-30 14:25:10 -0300

    feat: find schedue shift method

--- apps/api/src/modules/schedule-shifts/schedule-shifts.service.ts
+++ apps/api/src/modules/schedule-shifts/schedule-shifts.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common'
+import { ScheduleShiftRepository } from '@shared/database/repositories/schedule-shift.repositories'
 
 @Injectable()
-export class ScheduleShiftsService {}
+export class ScheduleShiftsService {
+  constructor(private readonly scheduleShiftsRepo: ScheduleShiftRepository) {}
+
+  async findFirst(id: string) {
+    return this.scheduleShiftsRepo.findFirst({ where: { id } })
+  }
+}
