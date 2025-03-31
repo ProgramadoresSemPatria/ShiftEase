a3c144605cbfb2ed0a71ae0a682f8f0e66aa6263
Lucas Franco <lcs.franco556@gmail.com>
2025-03-31 20:51:20 -0300

    feat: select exchange shift

--- apps/ui/src/components/SelectExchangeShift.tsx
+++ apps/ui/src/components/SelectExchangeShift.tsx
@@ -5,17 +5,31 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
+import type { ScheduleShift } from "@/types/schedule";
 
-export default function SelectExchangeShift() {
+type Props = {
+	shifts: ScheduleShift[];
+};
+
+export default function SelectExchangeShift({ shifts }: Props) {
+	console.log(shifts);
+	const formatDisplayDate = (date: string) => {
+		const dateFormated = new Date(date);
+		const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		return `${dayNames[dateFormated.getUTCDay()]} ${dateFormated.getUTCDate()}`;
+	};
+
 	return (
 		<Select>
 			<SelectTrigger className="w-full text-lg">
 				<SelectValue placeholder="Select" />
 			</SelectTrigger>
 			<SelectContent className="w-full text-lg">
-				<SelectItem value="shift1" className="text-lg">
-					Shift 1
-				</SelectItem>
+				{shifts.map((shift) => (
+					<SelectItem key={shift.id} value={shift.id} className="text-lg">
+						{formatDisplayDate(shift.date)}
+					</SelectItem>
+				))}
 			</SelectContent>
 		</Select>
 	);
