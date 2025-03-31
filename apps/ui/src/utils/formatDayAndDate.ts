34c2fa5a2ace41880486208a1fb7321392f73ec2
Lucas Franco <lcs.franco556@gmail.com>
2025-03-31 10:26:10 -0300

    fix: change parse date to locale utc date

--- apps/ui/src/utils/formatDayAndDate.ts
+++ apps/ui/src/utils/formatDayAndDate.ts
@@ -1,7 +1,7 @@
 export const formatDayAndDate = (dateStr: string): string => {
 	const date = new Date(dateStr);
 	const dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-	const dayOfWeek = dayNames[date.getDay()];
-	const dayOfMonth = date.getDate();
+	const dayOfWeek = dayNames[date.getUTCDay()];
+	const dayOfMonth = date.getUTCDate();
 	return `${dayOfWeek} ${dayOfMonth}`;
 };
