245b51d80d37530c53ac03a9ede711f5b9746cd3
Lucas Franco <lcs.franco556@gmail.com>
2025-03-31 22:52:08 -0300

    feat: signout button

--- apps/ui/src/components/DashboardHeader.tsx
+++ apps/ui/src/components/DashboardHeader.tsx
@@ -1,7 +1,12 @@
+import { AuthContext } from "@/contexts/auth/AuthContext";
 import { motion } from "motion/react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 export const DashboardHeader = () => {
+	const { signOut } = useContext(AuthContext);
+
 	return (
 		<div className="flex justify-between px-8 py-3 items-center border-b-orange-600 border-b-1 bg-zinc-950">
 			<Link to={"/"}>
@@ -14,6 +19,12 @@ export const DashboardHeader = () => {
 					Dashboard
 				</motion.div>
 			</Link>
+			<Button
+				onClick={signOut}
+				className="bg-orange-600 hover:bg-orange-700 text-white px-4 py-2 rounded"
+			>
+				Sign Out
+			</Button>
 		</div>
 	);
 };
