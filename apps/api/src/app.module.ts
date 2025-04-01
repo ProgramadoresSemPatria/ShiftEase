6402f338a1ae545ac47e5dd194a4f8ee612b0b05
Lucas Franco <lcs.franco556@gmail.com>
2025-03-31 23:41:55 -0300

    fix: deploy build

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,7 +6,9 @@ import { RoleGuard } from '@modules/users/roles/role.guard'
 import { UsersModule } from '@modules/users/users.module'
 import { Module } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { DatabaseModule } from '@shared/database/database.module'
+import { join } from 'path'
 import { AuthGuard } from './modules/auth/auth.guard'
 import { AuthModule } from './modules/auth/auth.module'
 
@@ -19,6 +21,9 @@ import { AuthModule } from './modules/auth/auth.module'
     SchedulesModule,
     ShiftsModule,
     ShiftExchangeRequestModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../..', 'ui', 'dist'),
+    }),
   ],
   controllers: [],
   providers: [
