166e8e12698e99913970bd22713f78ad36246e48
Lucas Franco <lcs.franco556@gmail.com>
2025-03-31 22:08:49 -0300

    feat: implement create swap on request swap dialog

--- apps/ui/src/components/RequestSwapDialog.tsx
+++ apps/ui/src/components/RequestSwapDialog.tsx
@@ -1,64 +1,109 @@
 import { Button } from "@/components/ui/button";
 import {
-	Dialog,
-	DialogClose,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
-import type { ScheduleShift } from "@/types/schedule";
+import { AuthContext } from "@/contexts/auth/AuthContext";
+import { useShiftExchangeRequest } from "@/hooks/useShiftExchangeRequest";
+import { ScheduleShift } from "@/types/schedule";
+import { useContext, useState } from "react";
 import SelectExchangeShift from "./SelectExchangeShift";
 
 type Props = {
-	name: string;
-	schedule: string;
-	day: string;
-	shiftId?: string;
-	loggedUserShifts: ScheduleShift[];
+  name: string;
+  schedule: string;
+  day: string;
+  receptorId: string;
+  destinationId?: string;
+  loggedUserShifts: ScheduleShift[];
 };
 
 export default function RequestSwapDialog({
-	name,
-	schedule,
-	day,
-	loggedUserShifts,
+  name,
+  schedule,
+  day,
+  receptorId,
+  destinationId,
+  loggedUserShifts,
 }: Props) {
-	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<Button variant="outline">Request Swap</Button>
-			</DialogTrigger>
-			<DialogContent className="sm:max-w-md">
-				<DialogHeader>
-					<DialogTitle className="font-bold text-xl">
-						Request Shift Swap
-					</DialogTitle>
-					<DialogDescription className="flex flex-col gap-3">
-						<div>
-							<div className="text-lg">{name}'s Shift</div>
-							<div className="flex border-1 px-2 py-5 justify-between rounded-md font-bold items-center text-lg">
-								<div className="text-black">{day}</div>
-								<div className="text-green-900">{schedule}</div>
-							</div>
-						</div>
-						<div>
-							<div className="text-lg">Selecione seu turno para oferecer:</div>
-							<SelectExchangeShift shifts={loggedUserShifts} />
-						</div>
-					</DialogDescription>
-				</DialogHeader>
-				<DialogFooter className="sm:justify-end">
-					<DialogClose>
-						<Button>Cancel</Button>
-					</DialogClose>
-					<Button className="bg-orange-500 text-white font-bold shadow-md">
-						Send Request
-					</Button>
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
-	);
+  const [selectedShiftId, setSelectedShiftId] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const { createSwapRequest } = useShiftExchangeRequest();
+  const { user } = useContext(AuthContext);
+
+  const handleSendRequest = async () => {
+    if (!selectedShiftId) {
+      setError("Por favor, selecione um turno para oferecer.");
+      return;
+    }
+    if (!destinationId) {
+      setError("Turno desejado inválido.");
+      return;
+    }
+
+    const swapRequestData = {
+      receptorId,
+      departmentId: user!.department_id,
+      originShiftId: selectedShiftId,
+      destinationId,
+    };
+
+    try {
+      await createSwapRequest(swapRequestData);
+      setError(null);
+      console.log("Solicitação de troca enviada com sucesso!");
+    } catch (err) {
+      setError("Erro ao enviar a solicitação de troca.");
+      console.error(err);
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Request Swap</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle className="font-bold text-xl">
+            Request Shift Swap
+          </DialogTitle>
+          <DialogDescription className="flex flex-col gap-3">
+            <div>
+              <div className="text-lg">{name}'s Shift</div>
+              <div className="flex border-1 px-2 py-5 justify-between rounded-md font-bold items-center text-lg">
+                <div className="text-black">{day}</div>
+                <div className="text-green-900">{schedule}</div>
+              </div>
+            </div>
+            <div>
+              <div className="text-lg">Selecione seu turno para oferecer:</div>
+              <SelectExchangeShift
+                shifts={loggedUserShifts}
+                onShiftSelect={(shiftId) => setSelectedShiftId(shiftId)}
+              />
+            </div>
+            {error && <div className="text-red-500">{error}</div>}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="sm:justify-end">
+          <DialogClose>
+            <Button>Cancel</Button>
+          </DialogClose>
+          <Button
+            className="bg-orange-500 text-white font-bold shadow-md"
+            onClick={handleSendRequest}
+          >
+            Send Request
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
